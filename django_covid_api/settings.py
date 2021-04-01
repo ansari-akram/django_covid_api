b2c9c9ff8ba405ffce6ae0c9c32c524a8e424d10
ansari-akram <ansariakramobaid@gmail.com>
2021-04-01 01:24:38 -0700

    Update settings.py

--- django_covid_api/settings.py
+++ django_covid_api/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ["*", "195.229.90.114"]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 1000
 }
 
 # Application definition
